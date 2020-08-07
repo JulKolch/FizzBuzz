267143a19b6a03ae81de64cbea33824ac47aca22
JulKolch <51914883+JulKolch@users.noreply.github.com>
2020-08-07 09:01:05 +0200

    Update FizzBuzzRun.java

--- FizzBuzzRun.java
+++ FizzBuzzRun.java
@@ -2,36 +2,62 @@ import java.util.Scanner;
 
 public class FizzBuzzRun {
 
+    private FizzBuzzGame fizzbuzzGame;
     static Scanner scanner = new Scanner(System.in);
+    int quantity;
+    boolean variation = askVariation();
 
-    public static void howMuch() {
-        System.out.println("How many numbers do you want to play with?");
-        System.out.print("> ");
+    public void main(String[] args) {
+        quantity = howMuch();
 
+        int[] factor = readNumbers();
+
+        String[] words = readWords();
+        fizzbuzzGame = new FizzBuzzGame(quantity, factor, words);
+        fizzbuzzGame.start();
+        fizzbuzzGame.ausgabe();
     }
 
-    static int quantity = scanner.nextInt();
+    private int howMuch() {
+        System.out.println("How many numbers do you want to play with?");
+        System.out.print("> ");
+        return scanner.nextInt();
+    }
 
-    static int[] factor = new int [quantity];
-
-    public static void readNumbers() {
+    private int[] readNumbers() {
+        int[] factor = new int[quantity];
         System.out.println("Geben sie die Zahlen ein");
-       for (int i = 0; i < quantity; i++){
+        for (int i = 0; i < quantity; i++) {
             System.out.print("> ");
             int input = scanner.nextInt();
             factor[i] = input;
         }
+        return factor;
     }
 
-    static String[] word = new String[quantity];
-
-    public static void readWords() {
+    private String[] readWords() {
+        String[] word = new String[quantity];
         System.out.println("Geben sie die Worte ein");
-
-        for (int i = 0; i < quantity; i++){
+        for (int i = 0; i < quantity; i++) {
             System.out.print("> ");
             String input = scanner.next();
             word[i] = input;
         }
+        return word;
+    }
+
+    boolean askVariation() {
+        System.out.println("Möchten Sie Variation aufrufen? [y oder n}");
+        String input = scanner.next();
+        if (input.equals("y") || input.equals("Y")) {
+            return true;
+        } else if (input.equals("n") || input.equals("N")) {
+            return false;
+        } else {
+            System.out.println("Falsche Eingabe, wird aber trotzdem aufgerufen");
+            return false;
+        }
+
+
     }
 }
