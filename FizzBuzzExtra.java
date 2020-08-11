b2436580a90b9094cf3ebb5e24e363bbcbd3df9b
JulKolch <51914883+JulKolch@users.noreply.github.com>
2020-08-11 14:05:13 +0200

    Update FizzBuzzExtra.java

--- FizzBuzzExtra.java
+++ FizzBuzzExtra.java
@@ -1,30 +1,90 @@
-public class FizzBuzzExtra {
+/**
+ * @Yulia - hier ein Serviervorschlag :-D
+ * public class FizzBuzzExtra extends FizzBuzzGame {
+ * <p>
+ * public FizzBuzzExtra(int laenge, int[] factor, String[] words, int variation) {
+ * super(laenge, factor, words, variation);
+ * }
+ * @Override public String[] start() {
+ * super.createArray();
+ * String[] ergebnis = doFizzBuzz();
+ * return ergebnis;
+ * }
+ * @Override protected String[] doFizzBuzz() {
+ * //@Yulia - hier kommt dann dein doVariationCode rein
+ * }
+ * }
+ */
 
-    public boolean variation = false;
-    public static String doVariation(String str, int[] factor, String[] word) {
+public class FizzBuzzExtra extends FizzBuzzGame {
 
-        String output = "";
+    private String[] array;
+    private int[] factor;
+    private String[] words;
+    private int variation;
 
-        for (int i = 0; i < factor.length; i++) {
-            int lange = str.length();
-            int ziffer = 0;
-            int zahl = Integer.parseInt(str);
-            while (lange > 0) {
-                ziffer = zahl % 10;
-                zahl = zahl / 10;
-
-                if (ziffer == factor[i]) {
-                    output += word[i];
-                }
-                lange--;
-            }
-        }
-        //Wenn Zufälle gefunden
-        if (!output.equals("")) {
-            output += "!!!";
-            str = output;
-        }
-        return str;
+    public FizzBuzzExtra(int laenge, int[] factor, String[] words, int variation) {
+        super(laenge, factor, words, variation);
     }
 
+
+    @Override
+    public String[] start() {
+        super.createArray();
+        String[] ergebnis = doFizzBuzz();
+        return ergebnis;
+    }
+
+    @Override
+    protected String[] doFizzBuzz() {
+        //@Yulia - hier kommt dann dein doVariationCode rein
+
+        for (int zahl = 0; zahl < array.length; zahl++) {
+            String element = array[zahl];
+            int elemInt = Integer.parseInt(element.trim());
+            String output = "";
+
+            for (int i = 0; i < factor.length; i++) {
+                if (elemInt % factor[i] == 0) {
+                    output += words[i];
+                }
+            }
+            //Wenn die Zahl ein Vielfache ist
+            if (!output.equals("")) {
+                array[zahl] = output;
+            }
+            //Wenn kein Vielfache, dann Variation (wenn erlaubt)
+            if (output.equals("")) {
+                for (int i = 0; i < factor.length; i++) {
+                    int lange = element.length();
+                    int ziffer = 0;
+                    int zahl1 = Integer.parseInt(element);
+                    while (lange > 0) {
+                        ziffer = zahl1 % 10;
+                        zahl1 = zahl1 / 10;
+
+                        if (ziffer == factor[i]) {
+                            output += words[i];
+                        }
+                        lange--;
+                    }
+                }
+                //Wenn Zufälle gefunden
+                if (!output.equals("")) {
+                    output += "!!!";
+                    array[zahl] = output;
+                }
+            }
+        }
+
+        return array;
+
+    }
+    void ausgabe() {
+        System.out.println("-- Die Ergebnisse --");
+        for (String s : array) {
+            System.out.println(s);
+        }
+    }
 }
+
