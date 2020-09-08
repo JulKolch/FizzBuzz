49b6cb9d6ca276e82435ff51a17f7b9ebcee261b
JulKolch <51914883+JulKolch@users.noreply.github.com>
2020-09-08 12:03:06 +0200

    Update FizzBuzzExtra.java

--- FizzBuzzExtra.java
+++ FizzBuzzExtra.java
@@ -1,3 +1,5 @@
+import static java.lang.Integer.parseInt;
+
 /**
  * @Yulia - hier ein Serviervorschlag :-D
  * public class FizzBuzzExtra extends FizzBuzzGame {
@@ -18,29 +20,36 @@
 
 public class FizzBuzzExtra extends FizzBuzzGame {
 
-
     public FizzBuzzExtra(int laenge, int[] factor, String[] words, int variation) {
         super(laenge, factor, words, variation);
     }
 
-
     @Override
     public String[] start() {
         super.createArray();
-        super.doFizzBuzz();
-        String[] ergebnis = doVariation(array);
+        array = super.doFizzBuzz();
+        String[] ergebnis = doVariation();
         return ergebnis;
     }
 
+    public boolean isNumeric(String s) {
+        return s != null && s.matches("[-+]?\\d*\\.?\\d+");
+    }
 
-    public String[] doVariation(String[] array) {
+    public String[] doVariation() {
 
-            //Wenn kein Vielfache, dann Variation (wenn erlaubt)
-            /*if (output.equals("")) {
+        //Wenn kein Vielfache, dann Variation
+        for (int zahl = 0; zahl < array.length; zahl++) {
+
+            //Nur für Zahlen in Array
+            if (isNumeric(array[zahl])) {
+                String element = array[zahl];
+                int elemInt = Integer.parseInt(element.trim());
+                output = "";
                 for (int i = 0; i < factor.length; i++) {
                     int lange = element.length();
                     int ziffer = 0;
-                    int zahl1 = Integer.parseInt(element);
+                    int zahl1 = elemInt;
                     while (lange > 0) {
                         ziffer = zahl1 % 10;
                         zahl1 = zahl1 / 10;
@@ -51,17 +60,20 @@ public class FizzBuzzExtra extends FizzBuzzGame {
                         lange--;
                     }
                 }
+
                 //Wenn Zufälle gefunden
                 if (!output.equals("")) {
                     output += "!!!";
                     array[zahl] = output;
                 }
-            }*/
 
+            }
+        }
 
         return array;
 
     }
+
     void ausgabe() {
         System.out.println("-- Die Ergebnisse --");
         for (String s : array) {
